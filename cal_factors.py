64ebe2e6599349f503b029c8f4d0a48e87a6416f
yichzhang2 <15921514331@163.com>
2018-09-03 22:54:02 +0800

    BTOP IC DONE

--- cal_factors.py
+++ cal_factors.py
@@ -1,28 +1,32 @@
 import pandas as pd
 import numpy as np
-import statsmodels.formula.api as smf
 import statsmodels.api as sm
-
-from datetime import datetime
-#import matplotlib.pyplot as plt
+from sklearn import preprocessing
 
 raw_data=pd.read_csv('../RawData/all_raw_data.csv',encoding='gbk')
 date=list(raw_data.groupby(by=['trade_date'],as_index=False).size().index)
 
 industry_dummies=pd.get_dummies(raw_data['Industry'])
-#raw_data= pd.concat([raw_data,industry_dummies],axis=1)
 
-raw_data['btop']=1/raw_data['pb']
 
 raw_data['lnCap']=np.log(raw_data['total_mv'])
+
+
+##########################以下是准备
+raw_data['btop']=1/raw_data['pb']
+
 X=pd.concat([raw_data['lnCap'],industry_dummies],axis=1)
 Y=raw_data['btop']
 
-result = sm.OLS(Y.head(50000),X.head(50000)).fit()
+Y=pd.concat([Y,X],axis=1)
+Y=pd.concat([raw_data['trade_date'],raw_data['adj_pct_change'],Y],axis=1)
 
-predict_window=10
-k=2 #过去5天的时间窗
-pb_ic = pd.DataFrame([])
+########################### 此时Y的结构是：日期，未来变化率，因子，中性化项目
+
+
+predict_window=10   #和adj_data中一致
+k=2                 #过去几日数据的时间窗
+btop_ic = pd.DataFrame([])
 
 #raw_industry=pd.get_dummies(raw_data)
 
@@ -30,23 +34,29 @@ for i in range(k-1,len(date)-predict_window):
     temp = pd.DataFrame([])
     print('solving date '+str(date[i]))
     for j in range(k):
-        temp=temp.append(raw_data[raw_data['trade_date']==date[i-j]])
+        temp=temp.append(Y[Y['trade_date']==date[i-j]])
 
-    #此处此处加
+    temp = temp.reset_index()
+    temp.drop(columns=['index'], inplace=True)
 
-    pb_ic=pb_ic.append([temp['pb'].corr(temp['pct_chg_shift'])])
+    nanblank = np.where(np.isnan(temp['btop']))
+    nanblank = list(nanblank[0])
+    temp = temp.drop(nanblank)
 
-pb_ic=pb_ic.reset_index()
-pb_ic.drop(columns=['index'],inplace=True)
+    temp['btop'] = preprocessing.scale(temp['btop'])
+    temp['lnCap'] = preprocessing.scale(temp['lnCap'])
+    result = sm.OLS(temp['btop'], temp.iloc[:, range(4, 32)]).fit()
+    temp['btop'] = result.resid
+
+    btop_ic=btop_ic.append([temp['btop'].corr(temp['adj_pct_change'])])
+
+btop_ic=btop_ic.reset_index()
+btop_ic.drop(columns=['index'],inplace=True)
 date_slice=pd.DataFrame(date[k-1:len(date)-predict_window])
 
 
-pb_ic=pd.DataFrame({'trade_date':date_slice[0],'IC':pb_ic[0]})
+btop_ic=pd.DataFrame({'trade_date':date_slice[0],'IC':btop_ic[0]})
 
-pb_ic.to_csv('../RawData/FactorIC.csv')
+btop_ic.to_csv('../RawData/BTOP_IC.csv')
 
 
-
-plt.figure()
-plt.plot(pb_ic['trade_date'],pb_ic_shift['ic'])
-plt.show()
