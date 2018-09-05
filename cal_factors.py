6ce88d06da8986c6b0b3c37f4474663ff3581cee
yichzhang2 <15921514331@163.com>
2018-09-05 12:56:53 +0800

    BTOP done

--- cal_factors.py
+++ cal_factors.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import statsmodels.api as sm
 from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression
 
 raw_data=pd.read_csv('../RawData/all_raw_data.csv',encoding='gbk')
 date=list(raw_data.groupby(by=['trade_date'],as_index=False).size().index)
@@ -19,7 +20,9 @@ X=pd.concat([raw_data['lnCap'],industry_dummies],axis=1)
 Y=raw_data['btop']
 
 Y=pd.concat([Y,X],axis=1)
-Y=pd.concat([raw_data['trade_date'],raw_data['adj_pct_change'],Y],axis=1)
+Y=pd.concat([raw_data['trade_date'],raw_data['pct_chg_shift'],Y],axis=1)
+
+Y.drop(['食品饮料'],axis=1, inplace=True)
 
 ########################### 此时Y的结构是：日期，未来变化率，因子，中性化项目
 
@@ -45,6 +48,7 @@ def winsorize(factor, std=3, have_negative = True):
 predict_window=10   #和adj_data中一致
 k=1                 #过去几日数据的时间窗
 btop_ic = pd.DataFrame([])
+btop_factor_premium = pd.DataFrame([])
 
 #raw_industry=pd.get_dummies(raw_data)
 
@@ -66,21 +70,27 @@ for i in range(k-1,len(date)-predict_window):
 
     temp['btop'] = preprocessing.scale(temp['btop'])
     temp['lnCap'] = preprocessing.scale(temp['lnCap'])
-    result = sm.OLS(temp['btop'], temp.iloc[:, range(4, 32)]).fit()
+    result = sm.OLS(temp['btop'], temp.iloc[:, range(4, 31)]).fit()
     temp['btop'] = result.resid
 
-    btop_ic=btop_ic.append([temp['btop'].corr(temp['adj_pct_change'],method='spearman')])
+    btop_ic=btop_ic.append([temp['btop'].corr(temp['pct_chg_shift'],method='spearman')])
+    linreg= sm.OLS(temp['btop'], temp['pct_chg_shift']).fit()
+
+    btop_factor_premium=btop_factor_premium.append([linreg.params[0]])
 
 btop_ic=btop_ic.reset_index()
 btop_ic.drop(columns=['index'],inplace=True)
+
+btop_factor_premium=btop_factor_premium.reset_index()
+btop_factor_premium.drop(columns=['index'],inplace=True)
+
 date_slice=pd.DataFrame(date[k-1:len(date)-predict_window])
 
+btop=pd.DataFrame({'trade_date':date_slice[0],'IC':btop_ic[0],'Factor_Premium':btop_factor_premium[0]})
 
-btop_ic=pd.DataFrame({'trade_date':date_slice[0],'IC':btop_ic[0]})
+print('positive percentage: '+str(btop[btop['IC']>0].count().IC/(len(date)-predict_window-k+1)))     #仅仅k=1时成立
+print('average IC: '+ str(btop['IC'].mean()))   #仅仅k=1时成立
 
-#print('positive percentage: '+str(btop_ic[btop_ic['IC']>0].count().IC/(len(date)-predict_window-k+1)))     #仅仅k=1时成立
-#print('average IC: '+ str(btop_ic['IC'].mean()))   #仅仅k=1时成立
-
-btop_ic.to_csv('../RawData/BTOP_IC.csv')
+btop.to_csv('../RawData/BTOP_IC.csv')
 
 
